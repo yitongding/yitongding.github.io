7d800f98294983224b23301a177210db1d2e7b5e
Yitong Ding <dyt111@gmail.com>
2019-11-17 22:59:28 -0500

    add tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-152754875-1`,
       },
     },
     `gatsby-plugin-feed`,
